9ed6e5ea7c1811e1018525d80213abf83a2338a7
feixiao <feixiao2020@sina.com>
2015-12-19 16:49:24 +0800

    update XMLConfigurationExample.go

--- examples/XMLConfigurationExample.go
+++ examples/XMLConfigurationExample.go
@@ -1,6 +1,9 @@
 package main
 
-import l4g "code.google.com/p/log4go"
+import (
+	l4g "github.com/feixiao/log4go"
+	"time"
+)
 
 func main() {
 	// Load the configuration (isn't this easy?)
@@ -10,4 +13,7 @@ func main() {
 	l4g.Finest("This will only go to those of you really cool UDP kids!  If you change enabled=true.")
 	l4g.Debug("Oh no!  %d + %d = %d!", 2, 2, 2+2)
 	l4g.Info("About that time, eh chaps?")
+
+	time.Sleep(1*time.Second)
+	l4g.Close()
 }
