ef0344df13bb1e33fb87c5f735712f874262877e
feixiao <feixiao2020@sina.com>
2015-12-19 16:00:17 +0800

    说明什么加了time.Sleep

--- examples/ConsoleLogWriter_Manual.go
+++ examples/ConsoleLogWriter_Manual.go
@@ -16,6 +16,8 @@ func main() {
 	log.Warn("Warn")
 	log.Error("Error")
 	log.Critical("Critical")
+	// ConsoleLogWriter 使用带缓存的chan进行输出的管理，所以不进行sleep操作
+	// 控制台没有输出，因为我们不知道什么时候channel会进行处理
 	time.Sleep(1*time.Second)
 	log.Close()
 }
