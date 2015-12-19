63fbd1c39091978d73a4167f75326d84d52f5208
feixiao <feixiao2020@sina.com>
2015-12-19 15:54:25 +0800

    update ConsoleLogWriter_Manual.go

--- examples/ConsoleLogWriter_Manual.go
+++ examples/ConsoleLogWriter_Manual.go
@@ -4,10 +4,18 @@ import (
 	"time"
 )
 
-import l4g "code.google.com/p/log4go"
+import l4g "github.com/feixiao/log4go"
 
 func main() {
-	log := l4g.NewLogger()
-	log.AddFilter("stdout", l4g.DEBUG, l4g.NewConsoleLogWriter())
-	log.Info("The time is now: %s", time.Now().Format("15:04:05 MST 2006/01/02"))
+	log := l4g.NewDefaultLogger(l4g.DEBUG)
+	log.Finest("Finest")
+	log.Fine("Fine")
+	log.Debug("Debug")
+	log.Trace("Trace")
+	log.Info("Info")
+	log.Warn("Warn")
+	log.Error("Error")
+	log.Critical("Critical")
+	time.Sleep(1*time.Second)
+	log.Close()
 }
